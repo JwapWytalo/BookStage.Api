55678374327311a0367e14540ae1898ea7f5fc68
JwapWytalo <wytalojwap@gmail.com>
2023-01-30 15:43:44 -0300

    add methods Post and Delete

--- src/main/java/com/apirest/crm/Controller/BookController.java
+++ src/main/java/com/apirest/crm/Controller/BookController.java
@@ -3,27 +3,47 @@ package com.apirest.crm.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.apirest.crm.Model.Cliente;
+import com.apirest.crm.Model.Book;
 import com.apirest.crm.Repository.BookRepository;
 
 @RestController
-@RequestMapping ("/books")
+@RequestMapping("/books")
 public class BookController {
-	
+
 	@Autowired
 	private BookRepository bookRepository;
-	
-	
+
 	@GetMapping
-	public List<Cliente> Listar(){
-		
+	public List<Book> Listar() {
+
 		return bookRepository.findAll();
+
+	}
+
+	@PostMapping
+	public Book adicionar(@RequestBody Book book) {
+
+		return bookRepository.save(book);
+
+	}
+	
+	@DeleteMapping("{id}")
+	
+	public String deletar(@PathVariable(value = "id") long bookId) {
+	
+		bookRepository.deleteById(null);
+		
+		return "Livro deletado!";
 		
 	}
 	
-	
 }
+
--- src/main/java/com/apirest/crm/Repository/BookRepository.java
+++ src/main/java/com/apirest/crm/Repository/BookRepository.java
@@ -3,9 +3,10 @@ package com.apirest.crm.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.apirest.crm.Model.Book;
 import com.apirest.crm.Model.Cliente;
 
 @Repository
-public interface BookRepository extends JpaRepository<Cliente, Long>{
+public interface BookRepository extends JpaRepository<Book, Long>{
 
 }
