113e3fd9a31e2607180238f502a18e41b1210308
JwapWytalo <wytalojwap@gmail.com>
2023-01-31 15:40:41 -0300

    add method put

--- src/main/java/com/apirest/crm/Controller/BookController.java
+++ src/main/java/com/apirest/crm/Controller/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,16 +35,33 @@ public class BookController {
 		return bookRepository.save(book);
 
 	}
-	
-	@DeleteMapping("{id}")
-	
-	public String deletar(@PathVariable(value = "id") long bookId) {
-	
-		bookRepository.deleteById(null);
-		
-		return "Livro deletado!";
-		
-	}
-	
-}
 
+	@PutMapping("/books/{id}")
+
+	public Book atualizar(@PathVariable(value = "id") long bookId, @RequestBody Book bookDetails) {
+
+		Book book = bookRepository.findById(bookId).orElse(null);
+
+		if (book != null) {
+
+			book.setName(bookDetails.getName());
+
+			bookRepository.save(book);
+
+		}
+
+		return book;
+
+	}
+
+	@DeleteMapping("/books/{id}")
+
+	public String deletar(@PathVariable(value = "id") long bookId) {
+
+		bookRepository.deleteById(bookId);
+
+		return "Livro deletado!";
+
+	}
+
+}
